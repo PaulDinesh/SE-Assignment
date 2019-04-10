bde2f502771247deb1a251f2b8712c21e03d9a34
Pauldinesh <Pauldinesh@gmail.com>
2019-04-10 23:36:12 +0100

    pre edited content

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -36,7 +36,59 @@ namespace WindowsFormsApp1
 
             //Get directories
             string[] files = Directory.GetFiles(source);
+            string str;
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t', ';' };
+            string[] List;
+            int j;
 
+            // string path = @"C:\Users\paul dinesh\Desktop\Data\data.txt";
+            StreamWriter sw = new StreamWriter(@"C:\Users\paul dinesh\Desktop\Data\Sort\sorted.txt");
+
+            foreach (string file in files)
+            {
+                StreamReader sr = new StreamReader(file);
+
+                //for (int i = 0; i < 2; i++)
+                // {
+                str = sr.ReadLine();
+                //     Console.WriteLine(Regex.Match(str, @"\d+.+\d").Value);
+                //     Console.WriteLine( "--------------");
+
+                //Spliting of line into words
+                List = str.Split(delimiterChars);
+                // }
+                //Sorting of the array
+                Array.Sort(List);
+                //No. of occurence
+                for (int i = 0; i < List.Length; i = j)
+                {
+                    string num = List[i];
+                    int c = 1;
+                    for (j = i + 1; j < List.Length; j++)
+                    {
+                        if (List[j] != num)
+                            break;
+                        else
+                            c++;
+                    }
+                    if (c == 1)
+                        //Console.WriteLine(num);
+                        sw.WriteLine(num);
+                    else
+                        //Console.WriteLine("{0},{1} ", num, c);
+                        sw.WriteLine("{0},{1} ", num, c);
+
+
+
+                }
+
+                sr.Close();
+
+
+
+            }
+            sw.Close();
+            Console.WriteLine(File.ReadAllText(@"C:\Users\paul dinesh\Desktop\Data\Sort\sorted.txt"));
             // FileInfo f1 = new FileInfo(source);
 
             foreach (string file in files)
