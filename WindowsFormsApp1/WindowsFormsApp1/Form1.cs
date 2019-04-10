f842509a672802afc3c9f4756474fc287001a418
Pauldinesh <Pauldinesh@gmail.com>
2019-04-10 23:30:44 +0100

    Updated master from Arden's Req 1 & 2

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace WindowsFormsApp1
 {
@@ -20,7 +21,43 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            //Folder Browser Dialog Box
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowDialog();
+            string source = fbd.SelectedPath;
+            string target = @"\sortedFiles";
+            target = string.Format("{0}{1}", source, target);
+
+            // If directory does not exist, create it. 
+            if (!Directory.Exists(target))
+            {
+                Directory.CreateDirectory(target);
+            }
+
+            //Get directories
+            string[] files = Directory.GetFiles(source);
+
+            // FileInfo f1 = new FileInfo(source);
+
+            foreach (string file in files)
+            {
+                string ext = Path.GetExtension(file);
+                //Checks for text file
+                if (ext == ".txt")
+                {
+                    //Assign the file name of that instance
+                    string name = Path.GetFileName(file);
+                    string newFileName = "sorted";
+                    //Asigning new name 
+                    newFileName = string.Format("{0}{1}", newFileName, name);
+
+                    //File copy 
+                    string sourceFile = Path.Combine(source, name);
+                    string destFile = Path.Combine(target, newFileName);
+                    File.Copy(sourceFile, destFile, true);
+                }
+            }
+
         }
     }
 }
