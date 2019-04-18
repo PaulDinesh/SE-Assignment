40d5a8ca0c08b988e344dc3895fb3df5046083d4
ar3en-d <arden.dias@gmail.com>
2019-04-18 20:29:31 +0100

    upgraded the form styling.

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -42,15 +42,18 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.button5 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.WindowText;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.button1.Location = new System.Drawing.Point(55, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(75, 42);
             this.button1.TabIndex = 0;
             this.button1.Text = "Browse";
             this.button1.UseVisualStyleBackColor = false;
@@ -59,9 +62,10 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Location = new System.Drawing.Point(29, 73);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(131, 38);
+            this.button2.Size = new System.Drawing.Size(131, 53);
             this.button2.TabIndex = 1;
             this.button2.Text = "Display Sorted Directories";
             this.button2.UseVisualStyleBackColor = false;
@@ -70,9 +74,10 @@
             // button3
             // 
             this.button3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button3.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.Location = new System.Drawing.Point(29, 271);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(131, 41);
+            this.button3.Size = new System.Drawing.Size(131, 53);
             this.button3.TabIndex = 2;
             this.button3.Text = "Display All Sorted Files";
             this.button3.UseVisualStyleBackColor = false;
@@ -81,33 +86,39 @@
             // listBox1
             // 
             this.listBox1.BackColor = System.Drawing.Color.SteelBlue;
-            this.listBox1.ForeColor = System.Drawing.Color.Maroon;
+            this.listBox1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ForeColor = System.Drawing.Color.Black;
             this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 17;
             this.listBox1.Location = new System.Drawing.Point(181, 57);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(575, 69);
+            this.listBox1.Size = new System.Drawing.Size(575, 72);
             this.listBox1.TabIndex = 3;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // listBox2
             // 
             this.listBox2.BackColor = System.Drawing.Color.SteelBlue;
-            this.listBox2.ForeColor = System.Drawing.Color.Maroon;
+            this.listBox2.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox2.ForeColor = System.Drawing.Color.Black;
             this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 17;
             this.listBox2.Location = new System.Drawing.Point(181, 155);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(575, 69);
+            this.listBox2.Size = new System.Drawing.Size(575, 72);
             this.listBox2.TabIndex = 4;
             this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
             // 
             // listBox3
             // 
             this.listBox3.BackColor = System.Drawing.Color.SteelBlue;
-            this.listBox3.ForeColor = System.Drawing.Color.Maroon;
+            this.listBox3.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox3.ForeColor = System.Drawing.Color.Black;
             this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 17;
             this.listBox3.Location = new System.Drawing.Point(181, 255);
             this.listBox3.Name = "listBox3";
-            this.listBox3.Size = new System.Drawing.Size(575, 69);
+            this.listBox3.Size = new System.Drawing.Size(575, 72);
             this.listBox3.TabIndex = 5;
             // 
             // textBox1
@@ -122,21 +133,24 @@
             // listBox4
             // 
             this.listBox4.BackColor = System.Drawing.Color.SteelBlue;
-            this.listBox4.ForeColor = System.Drawing.Color.Maroon;
+            this.listBox4.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox4.ForeColor = System.Drawing.Color.Black;
             this.listBox4.FormattingEnabled = true;
-            this.listBox4.Location = new System.Drawing.Point(181, 357);
+            this.listBox4.ItemHeight = 17;
+            this.listBox4.Location = new System.Drawing.Point(181, 363);
             this.listBox4.Name = "listBox4";
-            this.listBox4.Size = new System.Drawing.Size(575, 69);
+            this.listBox4.Size = new System.Drawing.Size(575, 72);
             this.listBox4.TabIndex = 7;
             this.listBox4.SelectedIndexChanged += new System.EventHandler(this.listBox4_SelectedIndexChanged);
             // 
             // button4
             // 
             this.button4.BackColor = System.Drawing.SystemColors.WindowText;
+            this.button4.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.button4.Location = new System.Drawing.Point(55, 501);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 37);
+            this.button4.Size = new System.Drawing.Size(93, 37);
             this.button4.TabIndex = 8;
             this.button4.Text = "Calculate";
             this.button4.UseVisualStyleBackColor = false;
@@ -146,9 +160,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.DimGray;
-            this.label1.Location = new System.Drawing.Point(178, 41);
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(178, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(237, 13);
+            this.label1.Size = new System.Drawing.Size(318, 18);
             this.label1.TabIndex = 9;
             this.label1.Text = "Lists all directories that have had text files sorted.";
             // 
@@ -156,9 +171,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.DimGray;
-            this.label2.Location = new System.Drawing.Point(178, 139);
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(178, 134);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(268, 13);
+            this.label2.Size = new System.Drawing.Size(356, 18);
             this.label2.TabIndex = 10;
             this.label2.Text = "Lists all files that have been sorted for a given directory.";
             // 
@@ -166,9 +182,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.DimGray;
-            this.label3.Location = new System.Drawing.Point(178, 239);
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(178, 234);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(251, 13);
+            this.label3.Size = new System.Drawing.Size(339, 18);
             this.label3.TabIndex = 11;
             this.label3.Text = "Lists all files that have been sorted for all directories.";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -177,15 +194,17 @@
             // 
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.DimGray;
-            this.label4.Location = new System.Drawing.Point(26, 390);
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(25, 383);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.Size = new System.Drawing.Size(58, 20);
             this.label4.TabIndex = 12;
             this.label4.Text = "Search";
             // 
             // button5
             // 
             this.button5.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button5.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.button5.Location = new System.Drawing.Point(681, 441);
             this.button5.Name = "button5";
@@ -195,6 +214,25 @@
             this.button5.UseVisualStyleBackColor = false;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(0, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(178, 339);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(343, 18);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Lists all files of searched word with no. of occurences.";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -202,6 +240,8 @@
             this.BackColor = System.Drawing.SystemColors.GrayText;
             this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.images;
             this.ClientSize = new System.Drawing.Size(800, 550);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -241,6 +281,8 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
